60ea2f67aa04ee50f6b391d0903e901bf5fbc0d9
Hordunlarmy <Hordunlarmy@gmail.com>
2024-12-13 03:21:14 +0100

    feat: Add database backup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -13,9 +13,9 @@ if ! php artisan migrate --force; then
 	echo "Migration failed, proceeding without stopping the container..."
 fi
 
-#if ! php artisan db:seed --force; then
-#	echo "Seeding failed, proceeding without stopping the container..."
-#fi
+if ! php artisan db:seed --force; then
+	echo "Seeding failed, proceeding without stopping the container..."
+fi
 
 # Index existing records in the database
 if ! php artisan search:index-existing; then
@@ -26,32 +26,32 @@ if ! php artisan create:superadmin "irep" "password"; then
 	echo "Creating superadmin failed, proceeding without stopping the container..."
 fi
 
-#if ! php artisan news:fetch; then
-#	echo "Fetching and Indexing news failed, proceeding without stopping the container..."
-#fi
+if ! php artisan news:fetch; then
+	echo "Fetching and Indexing news failed, proceeding without stopping the container..."
+fi
 
 # Clear the cache, routes, config, and views
 php artisan route:clear && php artisan config:clear && php artisan cache:clear && php artisan view:clear
 
 # Set permissions for the storage and bootstrap/cache directories
-# chown -R www-data:www-data storage bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
 
 # Start Supervisor to manage background processes
-# echo "Starting Supervisor..."
-# exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+echo "Starting Supervisor..."
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 
 # Start Reverb for broadcasting
-echo "Starting Reverb server..."
-php artisan reverb:start --debug --port=8090 &
+# echo "Starting Reverb server..."
+# php artisan reverb:start --debug &
 
 # Start Laravel queue worker
-echo "Starting Laravel queue worker..."
-php artisan queue:work --daemon &
+# echo "Starting Laravel queue worker..."
+# php artisan queue:work --daemon &
 
 # Tail the Laravel log
-echo "Tailing Laravel log..."
-tail -f storage/logs/laravel.log &
+# echo "Tailing Laravel log..."
+# tail -f storage/logs/laravel.log &
 
 # Run the Laravel application using artisan serve
-echo "Starting Laravel application..."
-exec php artisan serve --host=0.0.0.0 --port="$PORT"
+# echo "Starting Laravel application..."
+# exec php artisan serve --host=0.0.0.0 --port=8000
